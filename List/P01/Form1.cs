48f305786c80c77431c6dcbffb101ac5cfa91f10
novotnyv.03 <novotnyv.03@spst.eu>
2022-09-05 09:17:58 +0200

    P01 hotovo

--- List/P01/Form1.cs
+++ List/P01/Form1.cs
@@ -16,5 +16,45 @@ namespace P01
         {
             InitializeComponent();
         }
+        List<int> l = new List<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int pocet = Convert.ToInt32(textBox1.Text);
+            Random cisla = new Random();
+            int random_cislo = 0;
+            for (int i = 0; i<pocet; i++)
+            {
+                random_cislo = cisla.Next(1, 101);
+                l.Add(random_cislo);
+            }
+            for (int i = 0; i < l.Count; i++)
+            {
+                listBox1.Items.Add(l[i]);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            l.Sort();
+            string cisla = "";
+            for (int i = 0; i < l.Count; i++)
+            {
+                cisla += l[i]+ ", ";
+            }
+            MessageBox.Show(string.Format("{0}",cisla));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            l.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
